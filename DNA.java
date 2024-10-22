0f4b46f51f4e7dfa6909c848a3c9b79b4e77e0f9
soccer47 <105892798+soccer47@users.noreply.github.com>
2024-10-22 15:14:40 -0700

    Add files via upload

--- DNA.java
+++ DNA.java
@@ -17,12 +17,22 @@ public class DNA {
     /**
      * TODO: Complete this function, STRCount(), to return longest consecutive run of STR in sequence.
      */
+
+    // Array holding the values of the letters in the sequence at letterVals[ASCII value of letter]
+    private static int[] letterVals = new int[26];
+
+
     public static int STRCount(String sequence, String STR) {
+
+        // Set the values of the letters in letterVals
+        // A = 0, C = 1, G = 2, T = 3
+        letterVals[0] = 0;
+        letterVals[2] = 1;
+        letterVals[6] = 2;
+        letterVals[19] = 3;
 
         // Integer to hold length of STR
         int STRlength = STR.length();
-        // Int to hold various temporary values
-        int check;
         // Int to hold numerically converted sequence of length STRlength to be compared to the STR
         int posSTR = 0;
         // Int to hold numerical value of STR
@@ -35,8 +45,6 @@ public class DNA {
         int longestStreak = 0;
         // Long representing power of R that first digit in sequence being checked will be multiplied by
         int maxR = (int)Math.pow(R, STRlength - 1);
-        // ArrayList holding all the indexes where the STR appears in the sequence
-        ArrayList<Integer> indexes = new ArrayList<>();
 
 
         // Set numSTR to the numerical value of STR
@@ -58,19 +66,17 @@ public class DNA {
             // If the number matches the numerical value of STR and their strings match...
             // No need to compare String value of posSTR to STR because there's no overflow
             if (posSTR == numSTR) {
-                // Add the index of the STR match to the ArrayList
-                indexes.add(i - STRlength);
                 // If the current String is a match, add 1 to the currentStreak
                 currentStreak++;
                 // String to hold the next few characters to skip (not check for STR)
                 String nextSTR = "";
-                // Increment i according to the length of nextSTR
-                i += STRlength - 1;
                 // Make sure nextSTR won't go out of range of sequence
                 if (i + STRlength < sequence.length()) {
                     // Set nextSTR to the next few characters to skip
-                    nextSTR = sequence.substring(i - STRlength, i + 1);
+                    nextSTR = sequence.substring(i + 1, i + STRlength);
                 }
+                // Increment i according to the length of nextSTR
+                i += STRlength - 1;
                 // Skip the next few characters to see if the next possible STR is also the STR
                 for (int j = 0; j < nextSTR.length(); j++) {
                     // Cut off the first integer of posSTR if STRlength digits have been added
@@ -89,10 +95,6 @@ public class DNA {
                 currentStreak = 0;
             }
         }
-
-        // Print out the indexes where the STR appears in the sequence
-        System.out.println(longestStreak);
-
         // Return the longest streak of STRs in the sequence
         return longestStreak;
     }
@@ -101,17 +103,6 @@ public class DNA {
     // Return the numerical value of the inputted char
     public static int getCharVal(char c) {
         // A = 0, C = 1, G = 2, T = 3
-        if (c == 'A') {
-            return 0;
-        }
-        else if (c == 'C') {
-            return 1;
-        }
-        else if (c == 'G') {
-            return 2;
-        }
-        else {
-            return 3;
-        }
+        return letterVals[(int)c % 65 % 32];
     }
 }
